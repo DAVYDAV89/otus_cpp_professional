29d6a74c942dd42772677609018d8b13c235d745
Roman Davydov <davidov-corp@yandex.ru>
2023-12-28 11:46:07 +0300

    update main

--- home_work_1/main.cpp
+++ home_work_1/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main (int, char **) {
-    std::cout << "Version: " << version1() << std::endl;
+    std::cout << "Version: " << version() << std::endl;
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
