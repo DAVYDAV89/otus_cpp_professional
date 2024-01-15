ce731206ef3607da9907216e30913e5541e7402a
Roman Davydov <davidov-corp@yandex.ru>
2024-01-15 15:22:23 +0300

    lesson_2

--- main_test.cpp
+++ main_test.cpp
@@ -1,13 +1,43 @@
-#include "lib.h"
-#include <gtest/gtest.h>
+#include "ipAddres.h"
+#include "gtest/gtest.h"
 
-TEST(TestVersion, ValidVersion)
+TEST(IpAddressTest, ParseFromString)
 {
-  ASSERT_NE(version(), 100);
+    IpAddress ip;
+    ip.parseFromString("192.168.0.1");
+    EXPECT_EQ(ip.bytes[0], 192);
+    EXPECT_EQ(ip.bytes[1], 168);
+    EXPECT_EQ(ip.bytes[2], 0);
+    EXPECT_EQ(ip.bytes[3], 1);
 }
 
-int main(int argc, char **argv)
+//TEST(IpAddressTest, OperatorLessThan)
+//{
+//    IpAddress ip1;
+//    ip1.parseFromString("192.168.0.1");
+//    IpAddress ip2;
+//    ip2.parseFromString("192.168.0.2");
+//    EXPECT_TRUE(ip1 < ip2);
+//    EXPECT_FALSE(ip2 < ip1);
+//}
+
+TEST(IpAddressTest, OperatorGreaterThan)
 {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    IpAddress ip1; ip1.parseFromString("192.168.0.1");
+    IpAddress ip2; ip2.parseFromString("192.168.0.2");
+    EXPECT_TRUE(ip2 > ip1);
+    EXPECT_FALSE(ip1 > ip2);
+}
+
+TEST(IpAddressTest, OperatorEqual)
+{
+    IpAddress ip1; ip1.parseFromString("192.168.0.1");
+    IpAddress ip2; ip2.parseFromString("192.168.0.1");
+    EXPECT_TRUE(ip1 == ip2);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
